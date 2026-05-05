b5f89efa826ec76f7c7da067a75c9323d9bcd534
mdolancic <marko@dolancic.net>
2026-05-05 13:40:12 +0200

    add Oil plugin

--- init.lua
+++ init.lua
@@ -1,3 +1,15 @@
 require("config.options")
 require("config.keybinds")
 require("manage").setup()
+require("oil").setup({
+    columns = {
+        "icon",
+        "permissions",
+        "size"
+    },
+
+    preview_win = {
+        update_on_cursor_moved = true,
+        preview_method = "fast_scratch",
+    }
+})
--- lua/plugin-list.lua
+++ lua/plugin-list.lua
@@ -18,4 +18,15 @@ return {
     "mbbill/undotree",
     "ojroques/vim-oscyank",
     "captbaritone/better-indent-support-for-php-with-html",
+    {
+      'stevearc/oil.nvim',
+      ---@module 'oil'
+      ---@type oil.SetupOpts
+      opts = {},
+      -- Optional dependencies
+      dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+      -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+      -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+      lazy = false,
+    }
 }
